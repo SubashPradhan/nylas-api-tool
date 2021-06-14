9b2d0ad81e60b75bf100b4d339c28e8d43831273
Subash Pradhan <subash.webdeveloper@gmail.com>
2021-06-14 22:13:03 +0200

    Create links for the page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Component } from 'react';
 import { Route } from 'react-router-dom';
 import Main from './components/Main';
+import Email from './components/Email';
+
 class App extends Component {
 	render() {
 		return (
 			<>
 				<Route path="/" exact component={Main} />
+				<Route path="/email" component={Email} />
 			</>
 		);
 	}
---
+++ src/components/Email/view.js
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+export default function View() {
+	return (
+		<>
+			<div>Hello</div>
+			<Link to="/">Home</Link>
+		</>
+	);
+}
