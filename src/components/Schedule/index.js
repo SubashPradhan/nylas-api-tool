1ff562a23d5f64003083d1efd9f7278b5a528596
Subash Pradhan <subash.webdeveloper@gmail.com>
2021-06-21 23:08:37 +0200

    Inject scheduler cdn directly to index.html

--- src/components/Schedule/index.js
+++ src/components/Schedule/index.js
@@ -6,36 +6,30 @@ const nylas = window.nylas;
 
 class Schedule extends Component {
 	handleScheduleButton = async () => {
-		const nylasAccessToken = await this.props.accessToken;
-		// Invoke the schedule editor when a user clicks on the button
-		await nylas.scheduler.show({
-			auth: {
-				// Account access_token with active calendar scope
-				accessToken: `${nylasAccessToken}`,
-			},
-			style: {
-				// Style the schedule editor
-				tintColor: '#32325d',
-				backgroundColor: 'white',
-			},
-			defaults: {
-				event: {
-					title: '30-min Coffee Meeting',
-					duration: 30,
+		try {
+			const nylasAccessToken = await this.props.accessToken;
+			// Invoke the schedule editor when a user clicks on the button
+			await nylas.scheduler.show({
+				auth: {
+					// Account access_token with active calendar scope
+					accessToken: `${nylasAccessToken}`,
 				},
-			},
-		});
+				style: {
+					// Style the schedule editor
+					tintColor: '#32325d',
+					backgroundColor: 'white',
+				},
+				defaults: {
+					event: {
+						title: '30-min Coffee Meeting',
+						duration: 30,
+					},
+				},
+			});
+		} catch (error) {
+			console.log(error);
+		}
 	};
-
-	// Add nylas scheduler script when loading this page
-	componentDidMount() {
-		const script = document.createElement('script');
-		script.src =
-			'https://schedule.nylas.com/schedule-editor/v1.0/schedule-editor.js';
-		script.type = 'text/javascript';
-		script.async = true;
-		document.body.appendChild(script);
-	}
 
 	render() {
 		return (
