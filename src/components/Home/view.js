c00b3c89c6e6bd336f13e51f5e90acf5b39a3164
Subash Pradhan <subash.webdeveloper@gmail.com>
2021-08-25 22:07:15 +0200

    Working on styling page

--- src/components/Home/view.js
+++ src/components/Home/view.js
@@ -9,7 +9,7 @@ export default function View(props) {
 
 	const homePage = (
 		<div className="home">
-			<h1 className="title">Welcome to the Nylas API</h1>
+			<h1 className="main-title">Welcome to the Nylas API</h1>
 			<h2 className="sub-title"> Fetch every data with a click.</h2>
 			<div className="access-token-input-field">
 				<label className="access-token-label">Enter access token</label>
